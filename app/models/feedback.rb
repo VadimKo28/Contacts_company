cf79724132aa659e3df855b38acf57b8b6d2051c
Your Name <you@example.com>
2023-04-04 13:17:11 +0300

    Add validates feedback, styling feedback

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,7 @@
 class Feedback < ApplicationRecord
+  EMAIL_REGEXP = /\A[a-z0-9\.]+@[a-z0-9]+\.[a-z]+\z/
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEXP }
+  validates :message, presence: true
 end
