ad6bf61084429582b8c3fba7b522a8373a09859b
Your Name <you@example.com>
2023-04-21 16:11:06 +0300

    Add paginates contacts departament

--- app/controllers/departaments_controller.rb
+++ app/controllers/departaments_controller.rb
@@ -3,12 +3,17 @@ class DepartamentsController < ApplicationController
 
   def index
     @departaments = Departament.all
+    
   end
 
   def departament_search
     @departament = Departament.find(params[:search])
+    @contacts_departament = @departament.contacts.page(params[:page]).per(5) 
+
+
   rescue ActiveRecord::RecordNotFound
     flash[:notice] = "Вы не ввели значение"
     redirect_to root_url
   end
+
 end
